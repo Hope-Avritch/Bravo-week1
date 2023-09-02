33f9e6de273b49b0c84337a3f6cbb032ee44468a
Hope-Avritch <hopema66@gmail.com>
2023-09-02 12:20:01 -0400

    ex 13 started

---
+++ bravo-week 1/ex13.js
@@ -0,0 +1,19 @@
+/*Fill in the provided code template to let someone know which movie ratings they are able to see based on their age. People aged 17 and up can see any movie; people aged 13-16 can see PG-13, PG, or G movies, and everyone else can only see PG or G movies. */
+
+const age = 25;
+
+
+if (age >= 17) {
+    console.log(`You are ${age}. You are old enough to see any movie: R, PG-13, PG, or G.`);
+} else if (age >= 13) {
+    console.log "You can watch PG-13, PG or G movies"); //replace with your own code
+} else {
+    console.log(`You are ${age}. You are only old enough to see PG and G movies.`);
+}
+
+
+let myArray = [1, 2, 3, 4]
+myArray.pop();
+if (myArray.length 3) {
+
+}
