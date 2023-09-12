acb90a25769aa68cf81287e8f39a3984dc36306b
Hope-Avritch <hopema66@gmail.com>
2023-09-12 18:56:49 -0400

    saturday work

---
+++ bravo-week 1/week2d2.js/flex-grid-dem/worrdlefile/worlde.js
@@ -0,0 +1,15 @@
+let wordArr = ["about", "chair", "apple", "pizza", "grape"]
+
+document.addEventListener(load){
+    
+}
+
+
+
+
+function subitGuess(){
+    let userGuess = document.getElementById
+    {'userGuess'}.value;
+
+}
+
